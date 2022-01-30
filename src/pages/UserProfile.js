f336f31cad843981bc1845e7e3b6bdd3ac8fcc06
rohitb <rbhogal23@gmail.com>
2022-01-29 20:54:36 -0800

    text fiels sex, age, height, and weight

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,4 +1,41 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Paper, Typography } from '@mui/material';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+
+import SelectDropdown from '../components/SelectDropdown';
+import NumberTextField from '../components/text-fields/NumberTextField';
+import HeightTextField from '../components/text-fields/HeightTextField';
+
+function createData(label, input) {
+  return { label, input };
+}
+
+const renderSexInput = () => {
+  return <SelectDropdown />;
+};
+
+const rows = [
+  { label: 'Sex', input: renderSexInput },
+  createData('Goal', 237),
+  createData('Fat Loss Deficit/Muscle Gain (%)', 262),
+  createData('Cupcake', 305),
+  createData('Gingerbread', 356),
+];
+
+const selectSexData = {
+  label: 'Sex',
+  selectItems: ['Male', 'Female'],
+};
+
+const selectAgeData = {
+  label: 'Age',
+  selectItems: Array.from(Array(10).keys()),
+};
 
 const UserProfile = () => {
   return <div>UserProfile</div>;
